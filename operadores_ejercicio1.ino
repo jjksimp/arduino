5106b06935259fb5c192f1c644bbf4b878255406
jjksimp <90753482+jjksimp@users.noreply.github.com>
2021-11-03 12:47:23 +0100

    Add files via upload

---
+++ operadores_ejercicio1.ino
@@ -0,0 +1,36 @@
+
+int estadoBotonPin2 = 0;
+int estadoBotonPin3 = 0;
+void setup () {
+ 
+pinMode(2, INPUT);
+pinMode(3, INPUT);
+pinMode(4, OUTPUT);
+pinMode(2, OUTPUT);
+Serial.begin(9600);
+}
+
+void loop() {
+ estadoBotonPin2 = digitalRead(2);
+ estadoBotonPin3 = digitalRead(3);
+ Serial.print("Estado del botón 2: ");
+ Serial.println(estadoBotonPin2);
+ Serial.print("Estado del botón 3: ");
+ Serial.println(estadoBotonPin3);
+
+if (estadoBotonPin2 == HIGH
+  &&
+ estadoBotonPin3 == HIGH){
+digitalWrite(4, HIGH); 
+digitalWrite(5, HIGH); 
+
+
+}
+
+else {
+  
+digitalWrite(4, LOW); 
+digitalWrite(5, LOW); 
+
+}
+}// llave del loop
